d815deeba80eebe31906638fc59fee45839f665f
Totter <tottercotter@gmail.com>
2022-08-15 14:00:12 +0700

    test

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let popup = document.querySelector('.pop-up');
+let popupForm = popup.querySelector('.pop-up__form');
+let editButton = document.querySelector('.profile__edit-button');
+let nameInput = popupForm.querySelector('.pop-up__input_type_name');
+let jobInput = popupForm.querySelector('.pop-up__input_type_job');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let closeButton = popup.querySelector('.pop-up__close-button');
+
+function openPopup() {
+  FormValue();
+  popup.classList.add('pop-up_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('pop-up_opened');
+}
+
+function FormValue() {
+  nameInput.setAttribute('value', profileName.textContent);
+  jobInput.setAttribute('value', profileJob.textContent);
+}
+
+function formSubmitHand(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', formSubmitHand);
